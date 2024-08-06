ae677c327bd2136ea0f53a5365aaec76ef13659c
Mohammad Riad Chaar <Mhdriadchaar22@gmail.com>
2024-08-06 23:49:55 +0300

    2:21:50 time youtube

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDB _db;
+        public CategoryController(ApplicationDB db) 
+        { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyWeb/Data/ApplicationDB.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDB : DbContext
+    {
+        public ApplicationDB(DbContextOptions<ApplicationDB> options) : base(options) 
+        {
+               
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "action", Display = 1 },
+                new Category { CategoryId = 2, Name = "action", Display = 2 },
+                new Category { CategoryId = 3, Name = "action", Display = 3 }
+                
+                );
+        }
+    }
+}
